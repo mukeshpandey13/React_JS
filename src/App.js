e12114f11a05f264988aefd3fa7be6ef55b8224c
Pande Mukesh <pandeymukesh13>
2024-11-30 15:19:52 +0545

    #5 - Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
 
 function App() {
-  const title = "This is my first web blog"
-  const like = 50;
-  const link="https://www.google.com";
-
-
-
   return (
     <div className="App">
+
+      {/* Navbar is imported from Navabar.js  */}
+      <Navbar/>
+
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {like} times.</p>
-
-        {/*  we can able to write js code in under {}  */}
-        <p>{10}</p>
-        <p>{"Hello world"}</p>
-        <p>{[1,2,343,45,23]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google site</a>
+        <Home/>
       </div>
+
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="Navbar">
+            <h1>My Blog</h1>
+            <div className="links"></div>
+            <a href="/">Home</a>
+            <a href="/">New Blog</a>
+        </nav>
+     );
+}
+  
+// using export we can use navbar in other page 
+export default Navbar;
