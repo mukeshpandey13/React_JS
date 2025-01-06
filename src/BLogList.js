d5a69f0b516e72f7e5ef7ecf64956ca36f560e3b
Pande Mukesh <pandeymukesh13>
2025-01-06 11:06:19 +0545

    #17 - Fetching Data with useEffect

--- src/BLogList.js
+++ src/BLogList.js
@@ -1,28 +1,19 @@
-const BlogList = ({blogs,title,btnHandler}) => {
+const BlogList = ({ blogs, title }) => {
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
 
-    // use props in bloglist parameter 
-    // const blogs = props.blogs;
-    // const title = props.title;
+      {/* Iterate over the `blogs` array using the `map` function to generate JSX for each blog */}
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          {/* Display the blog title */}
+          <h2>{blog.title}</h2>
 
-    return ( 
-        <div className="blog-list">
-            <h2>{title}</h2>
-
-              {/* Iterate over the `blogs` array using the `map` function to generate JSX for each blog */}
-              {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-
-                    {/* Display the blog title */}
-                    <h2>{blog.title}</h2>
-
-                    {/* Display the author of the blog */}
-                    <p>Written by {blog.author}</p>
-
-                    {/* to delete list  */}
-                    <button onClick={()=>btnHandler(blog.id)}>Delete</button>
-                </div>
-            ))}
+          {/* Display the author of the blog */}
+          <p>Written by {blog.author}</p>
         </div>
-     );
-}
+      ))}
+    </div>
+  );
+};
 export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,56 +1,26 @@
 import React from "react";
-import { useState,useEffect } from "react"; // Import the useState hook to manage state in a functional component
+import { useState, useEffect } from "react";
 import BlogList from "./BLogList";
 
 const Home = () => {
-  // Initialize the `blogs` state with an array of blog objects using the `useState` hook
-  //blogs is an array that contains multiple objects
-  const [blogs, setBlogs] = useState([
-    {
-      // we use object
-      title: "My new website", // Title of the blog
-      body: "lorem ipsum...", // Placeholder content
-      author: "Mukesh", // Name of the blog author
-      id: "1", // Unique ID for each blog (important for mapping)
-    },
-    {
-      title: "Welcome party",
-      body: "lorem ipsum...",
-      author: "Marcus",
-      id: "2",
-    },
-    {
-      title: "Web dev tips",
-      body: "lorem ipsum...",
-      author: "Harry",
-      id: "3",
-    },
-  ]);
-
-//   btn function to delete blogs 
-  const btnHandler = (id) => {
-    const newblogs = blogs.filter((blog) => blog.id !== id); // Filter out the blog with the matching id
-    setBlogs(newblogs); // Update the state with the filtered blogs
-  };
-
-  const [name,setName]= useState("Marcus");
+  const [blogs, setBlogs] = useState(null);
 
   // useEffect
-   useEffect(() => {
-    console.log('use effect run')
-    console.log(name)
-  },[name]);
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+
+      .then((data) => {
+        // console.log(data);
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home" id="home">
-      <BlogList blogs={blogs} title="All blogs" btnHandler={btnHandler} />
-      {/* <BlogList
-        blogs={blogs.filter((blog) => blog.author == "Mukesh")}
-        title="Mukesh blogs"
-      /> */}
-
-      <h3>{name}</h3>
-      <button onClick={()=>setName("Mukesh pandey")}>Change Name</button>
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
 };
